6db2ec1b78eb5b88c19bef4f2efdd742c9cab866
Nick Qi <fintie@gmail.com>
2021-08-05 18:00:30 +1000

    Create aboutUs.js

---
+++ src/contents/aboutUs.js
@@ -0,0 +1,62 @@
+const aboutUs = `
+<h1>Mission</h1>
+
+Upcrowd’s mission is to drive social impact and empower people’s success by providing a simplified digital marketplace platform for everyone to access a wide range of mentors and industry experts. We leverage AI to efficiently and accurately match mentors and mentees anywhere in the world.
+
+
+<h1>How It Works</h1>
+
+<h2>For Mentors</h2>
+
+UpCrowd is the place where successful entrepreneurs, senior executives, and leading experts make extra income sharing their wisdom with the next generation of achievers.
+<br>
+Step 1: Create a Profile
+<br>
+Tell the world about your areas of expertise, what you’re passionate about and how you can help mentees.
+<br>
+Step 2: Accept Mentees 
+<br>
+Our advanced AI matching system helps the mentees who would most benefit from your expertise find you and request to be your mentee. 
+<br>
+Step 3: Mentoring Sessions
+<br>
+You can choose your preferred style of support from messenger chat, phone or video sessions with the flexibility to fit your busy schedule and give your mentee advice and feedback on their ideas.
+<br>
+Step 4: Get Paid
+<br>
+You decide your own monthly mentoring fee, and we take care of invoicing and payments. 
+
+<h2>For Mentees</h2>
+
+Our mentors are successful and experienced business leaders passionate about giving back by passing on their wisdom and insights. Here’s how they can help:
+
+
+Step 1: Business & Startups
+
+<br>
+
+Instant access to seasoned entrepreneurs and business leaders with insights to help you grow faster, overcome obstacles, and validate your ideas.
+
+<br>
+
+Step 2: Career<br>
+
+Work smarter not harder to actually get ahead with personalised insider advice to grow your career. Mentors can help you get promoted or hired, navigate office politics, and deal with workplace conflicts and employee relations.
+
+<br>
+
+Step 3: Technical Specialists
+
+<br>
+
+Whether you're a technical professional wanting to level up your skills, or are working on your own project and need ad hoc advice and troubleshooting on your work to get you unstuck, mentoring from a specialist expert can get you there faster!
+
+<br>
+
+Step 4: Mind
+
+<br>
+
+Get what you want out of life with qualified coaches in mental health, diet and nutrition, personal growth, overcoming limiting beliefs, networking and relationships, planning and goal setting to make your dreams a reality.`
+
+export default aboutUs;
