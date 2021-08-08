9d873e92f1a83e4eea9ceeb2358c86788158f589
Nick Qi <fintie@gmail.com>
2021-08-08 15:09:35 +1000

    Update MentorsList.js

--- src/components/MentorsList/MentorsList.js
+++ src/components/MentorsList/MentorsList.js
@@ -64,8 +64,9 @@ const MentorsList = props => {
       {nothingToShow(!!mentorsInList.length)}
     </InfiniteScroll>
     
+    <a href="https://forms.gle/wPzUKy1HtaK9UMvr8" class="patreon-link" aria-label="View More" rel="noopener noreferrer" target="_blank"><img src="https://github.com/fintie/upcrowd-web/blob/master/public/viewmore_button.png?raw=true" alt="View More"></a>
+    
     </section>
-    <a href="https://forms.gle/wPzUKy1HtaK9UMvr8" class="patreon-link" aria-label="View More" rel="noopener noreferrer" target="_blank"><img src="https://github.com/fintie/upcrowd-web/blob/master/public/viewmore_button.png?raw=true" alt="View More"></img></a>
   );
 };
 
